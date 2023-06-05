924c8951fc82f20ea92fc0831eecf7082934e90a
João Paulo Araújo <jpf.araujo99@hotmail.com>
2023-06-05 20:15:37 -0300

    commit ativando itens do orçamento

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,26 @@
-const links = document.querySelectorAll('.header-menu a');
+// Ativar links do Menu
+const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
-    const url = location.href;
-    const href = link.href;
+  const url = location.href;
+  const href = link.href;
 
-    if(url.includes(href)) {
-        link.classList.add('ativo');
-    }
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
 }
 
 links.forEach(ativarLink);
+
+// Ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
