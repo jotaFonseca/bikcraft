76a3e6abc08266adc1411f8c0e317b7ce6f66800
João Paulo Araújo <jpf.araujo99@hotmail.com>
2023-06-06 12:25:15 -0300

    otimizado para producao

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,25 +25,46 @@ function ativarProduto(parametro) {
 
 parametros.forEach(ativarProduto);
 
-
 // Perguntas Frequentes
 
-const perguntas = document.querySelectorAll('.questions button');
+const perguntas = document.querySelectorAll(".questions button");
 
 function ativarPergunta(e) {
-    const pergunta = e.currentTarget;
-    const controls = pergunta.getAttribute('aria-controls');
-    const resposta = document.getElementById(controls);
+  const pergunta = e.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
 
-    resposta.classList.toggle('ativa');
-    const ativa = resposta.classList.contains('ativa');
-    pergunta.setAttribute('aria-expanded', ativa);
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
 }
 
 function eventoPerguntas(pergunta) {
-    pergunta.addEventListener('click', ativarPergunta);
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
 perguntas.forEach(eventoPerguntas);
 
+// Galeria de Bicicleta
 
+const galeria = document.querySelectorAll(".bikes-img img");
+const galeriaContainer = document.querySelector(".bikes-img");
+
+function trocarImg(e) {
+  const img = e.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventoGaleria(img) {
+  img.addEventListener("click", trocarImg);
+}
+
+galeria.forEach(eventoGaleria);
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
